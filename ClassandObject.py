ae2c281e1e1a1e5ba4f27d58b1ff0af897e11acd
Dipti Singh <diptisinghnsp@gmail.com>
2026-08-27 12:25:52 +0000

    ATM Machine Program Using Class and Object in Python

---
+++ ClassandObject.py
@@ -0,0 +1,4 @@
+class Atm:
+    def __init__(self):
+        self.pin = ''
+        self.balance = 0
---
+++ OOP_One.py
@@ -0,0 +1,30 @@
+print("Generality Of Specificity")
+print("using OOP we will create our datatype")
+print("""
+Object 
+Class
+Polymorphism
+Encapsulation
+Inheritance
+Abstraction
+""")
+print("Class and Object")
+
+l = [1,2,3]
+print(type(l))
+print(l.upper())
+print("""
+Object is instance of Class 
+class
+  |
+Object
+
+we create object of class 
+
+objectname = classname()
+
+class have data and function 
+
+Class name is Pasacal Case (MyAtm)
+
+""")
